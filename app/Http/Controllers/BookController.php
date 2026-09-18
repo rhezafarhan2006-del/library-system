7fc70e958a00af84be9596ba8fc1c4f0a66db8a8
rhezashahz <rhezafarhan2006@gmail.com>
2026-09-18 17:11:42 +0700

    Add books pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,8 +6,46 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
-    {
-        return view('book.index');
+    public function index(){
+        $title = 'Daftar Buku';
+        $description = 'Daftar buku yang tersedia di perpustakaan';
+        $books = [
+            'Pemrograman PHP',
+            'Laravel untuk pemula',
+            'Basis Data',
+            'Algoritma dan Pemrograman',
+            'Pemrograman Berorientasi Objek',
+            'AI untuk Dunia',
+            'Ultraman',
+            'NasionalIsMe'
+        ];
+ 
+        $authors = [
+            'Ahmad Fauzi',
+            'Dewi Lestari',
+            'Budi Raharjo',
+            'Siti Nurhaliza',
+            'Rian Ardiansyah',
+            'Putri Wulandari',
+            'Eko Prasetyo',
+            'Maya Kartika',
+        ];
+
+        $years = [
+            2015,
+            2018,
+            2020,
+            2012,
+            2023,
+            2017,
+            2019,
+            2021,
+        ];
+
+        $stock = 7;
+        return view('book.index' , compact('title', 'description', 'books','authors','years' , 'stock'));
     }
+    public function show ($id){ 
+            return 'ID Buku' . $id;
+        }
 }
--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,2 +1,23 @@
-<h1> Daftar Buku </h1>
-<p> Sistem Informasi Perpustakaan </p>
+@extends('layouts.app')
+
+@section('title' , 'Daftar Buku')
+
+@section('content')
+<h2>Daftar Buku</h2>
+<ul>
+    @foreach($books as $key => $book)
+        <li>
+            <strong>Judul:</strong>{{$book}} <br>
+            <strong>Penulis:</strong>{{$authors[$key]}} <br>
+            <strong>Tahun:</strong>{{$years[$key]}} <br>
+
+        </li>
+    @endforeach
+</ul>
+
+@if($stock > 0)
+    <p>Stock tersedia : {{$stock}}</p>
+@else
+    <p>Buku sedang habis.</p>
+@endif
+@endsection
